5c7a84a51f71a39d0badcd519482b6e7b20d80be
iamtrimax <trimax2k3@gmail.com>
2025-08-18 22:10:51 +0700

    setup

--- travel/src/App.jsx
+++ travel/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Footer from './componets/Footer/Footer'
+import Header from './componets/Header/Header'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  
+
   return (
     <>
-      
+      <Header />
+      <main className='main'>
+        <Outlet />
+      </main>
+      <Footer/>
     </>
   )
 }
---
+++ travel/src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home/Home'
+import App from '../App'
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      }
+    ]
+  }
+])
+export default Routes
