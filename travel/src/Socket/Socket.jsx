afa8bdcea5467f79af1b6307e0697fa90de33183
iamtrimax <trimax2k3@gmail.com>
2025-11-11 15:54:14 +0700

    tessstttt

--- travel/src/Socket/Socket.jsx
+++ travel/src/Socket/Socket.jsx
@@ -1,7 +1,7 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://api.webtravel.click", {
   autoConnect: false,
 }); // backend URL
 export const connectSocket = () => {
